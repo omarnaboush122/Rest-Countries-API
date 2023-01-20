57fe1f634e97e35e67f5df7e59293e8cb50edb9a
omarnaboush122 <omarnaboush89@gmail.com>
2023-01-20 07:52:38 -0800

    add notfound page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Countries/>}/>
+        <Route path="*"  />
       </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+
+
+const NotFound = () => {
+  return (
+    <div>
+      Sorry not found
+    </div>
+  );
+}
+
+export default NotFound;
