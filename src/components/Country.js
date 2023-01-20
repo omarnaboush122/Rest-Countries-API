d96178fd8e8ecfdfb34a21416c2b0275f8c19ba8
omarnaboush122 <omarnaboush89@gmail.com>
2023-01-20 08:16:53 -0800

    add data to each country

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,9 +1,15 @@
 
 
-const Country = () => {
+const Country = ({flags,name,population,region,subregion}) => {
   return (
     <div>
-      
+      <img src={flags.svg} alt={name.common} />
+      <h2>{name.common}</h2>
+      <ul>
+        <li>Population: {population.toLocaleString()}</li>
+        <li>Region: {region}</li>
+        <li>Subregion: {subregion}</li>
+      </ul>
     </div>
   );
 }
