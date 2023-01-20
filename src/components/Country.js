21ec15d3b5f643833dc66f6fe2bf962c5134153f
omarnaboush122 <omarnaboush89@gmail.com>
2023-01-20 10:57:58 -0800

    get data for each country at a single page

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,22 +1,27 @@
 import { Link } from "react-router-dom";
 
-
-const Country = ({flags,name,population,region,subregion}) => {
+const Country = ({ flags, name, population, region, subregion }) => {
   return (
-  <Link to={`/${name.common}`}>
-    <div className="bg-white hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700 transition-all duration-200 rounded-lg shadow overflow-hidden">
-      <img className="w-full object-cover  md:h-72" src={flags.svg} alt={name.common} />
-      <div className="p-4">
-      <h2 className="font-bold text-lg text-gray-900 dark:text-white mb-2">{name.common}</h2>
-      <ul className="flex flex-col gap-2 dark:text-gray-400">
-        <li>Population: {population.toLocaleString()}</li>
-        <li>Region: {region}</li>
-        <li>Subregion: {subregion}</li>
-      </ul>
+    <Link to={`/${name.common}`}>
+      <div className="bg-white hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700 transition-all duration-200 rounded-lg shadow overflow-hidden">
+        <img
+          className="w-full object-cover  md:h-72"
+          src={flags.svg}
+          alt={name.common}
+        />
+        <div className="p-4">
+          <h2 className="font-bold text-lg text-gray-900 dark:text-white mb-2">
+            {name.common}
+          </h2>
+          <ul className="flex flex-col gap-2 dark:text-gray-400">
+            <li>Population: {population.toLocaleString()}</li>
+            <li>Region: {region}</li>
+            <li>Subregion: {subregion}</li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </Link>
+    </Link>
   );
-}
+};
 
 export default Country;
--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,11 +1,26 @@
-
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const CountryDetails = () => {
-  return (
-    <div>
-      Country Details
-    </div>
-  );
-}
+  const [countryData, setCountryData] = useState([]);
+  const { name } = useParams();
+
+  useEffect(() => {
+    const getCountry = async () => {
+      try {
+        const response = await fetch(
+          `https://restcountries.com/v3.1/name/${name}`
+        );
+        const data = await response.json();
+        setCountryData(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getCountry();
+  }, [name]);
+
+  return <div className="text-white">{name}</div>;
+};
 
 export default CountryDetails;
