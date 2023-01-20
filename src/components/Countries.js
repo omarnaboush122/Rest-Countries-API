73e59007abc2411f12bd79a21114d9f1d467a229
omarnaboush122 <omarnaboush89@gmail.com>
2023-01-20 08:02:02 -0800

    fetch all data countries from the api

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,11 +1,23 @@
-
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Countries = () => {
-  return (
-    <>
-      Countries
-    </>
-  );
-}
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountries = async () => {
+      try {
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        const data = await response.json();
+        setCountries(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getCountries();
+  }, []);
+  
+  return <>Countries</>;
+};
 
 export default Countries;
