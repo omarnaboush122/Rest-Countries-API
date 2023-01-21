3254b4b5c2f6c58bafa38b57e0d8327e01bae299
omarnaboush122 <omarnaboush89@gmail.com>
2023-01-20 17:06:07 -0800

    add search country function

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -3,6 +3,48 @@ import Country from "./Country";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+  const [input, setInput] = useState({
+    search: "",
+    filterbyregion: "",
+  });
+
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    setInput((prevInput) => ({
+      ...prevInput,
+      [name]: value,
+    }));
+  };
+
+   const searchCountry = async () => {
+    try {
+      const response = await fetch(`https://restcountries.com/v3.1/name/${input.search}`)
+      const data = await response.json();
+      setCountries(data);
+    } catch(error) {
+      console.error(error);
+    }
+  }
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    searchCountry();
+  }
+
+  const filterCountry = async () => {
+    try {
+      const response = await fetch(`https://restcountries.com/v3.1/region/${input.filterbyregion}`)
+      const data = await response.json();
+      setCountries(data)
+    } catch(error) {
+      console.error(error);
+    }
+  }
+
+  const handleFilter = (e) => {
+    e.preventDefault();
+    filterCountry();
+  }
 
   useEffect(() => {
     const getCountries = async () => {
@@ -17,40 +59,54 @@ const Countries = () => {
     getCountries();
   }, []);
 
+  
+
   const allCountries = countries.map((country) => (
     <Country key={country.name.common} {...country} />
   ));
 
   return (
     <>
-  <div className="container mx-auto p-8">
-  <div className="flex flex-col gap-4 mb-8 md:flex-row md:items-center md:justify-between">
-      <form autoComplete="off" className="max-w-3xl md:flex-1">
-      <input className="py-3 px-4 text-gray-600 placeholder-gray-600 w-full rounded shadow outline-none dark:text-gray-400 dark:placeholder:text-gray-400 dark:bg-gray-800 dark:focus:bg-gray-700 transition-all duration-200" type="text" name="search" placeholder="Search for a country by its name" required />
-      </form>
-      <form>
-        <select name="filter-by-region" className="w-52 py-3 px-4 outline-none shadow rounded text-gray-600 dark:text-gray-400 dark:bg-gray-800 focus:bg-gray-700">
-          <option value="All">All</option>
-          <option value="Europe">Europe</option>
-          <option value="Asia">Asia</option>
-          <option value="Afria">Africa</option>
-          <option value="Oceania">Oceania</option>
-          <option value="Americas">Americas</option>
-          <option value="Antarctic">Antarctic</option>
-
-        </select>
-      </form>
-    </div>
-    {countries ? (
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
-          {allCountries}
+      <div className="container mx-auto p-8">
+        <div className="flex flex-col gap-4 mb-8 md:flex-row md:items-center md:justify-between">
+          <form autoComplete="off" className="max-w-3xl md:flex-1" onSubmit={handleSearch}>
+            <input
+              className="py-3 px-4 text-gray-600 placeholder-gray-600 w-full rounded shadow outline-none dark:text-gray-400 dark:placeholder:text-gray-400 dark:bg-gray-800 dark:focus:bg-gray-700 transition-all duration-200"
+              type="text"
+              name="search"
+              placeholder="Search for a country by its name"
+              required
+              onChange={handleChange}
+              value={input.search}
+            />
+          </form>
+          <form onSubmit={handleFilter}>
+            <select
+              name="filterbyregion"
+              className="w-52 py-3 px-4 outline-none shadow rounded text-gray-600 dark:text-gray-400 dark:bg-gray-800 dark:focus:bg-gray-700"
+              onChange={handleChange}
+              value={input.filterbyregion}
+            >
+              <option value="All">All</option>
+              <option value="Europe">Europe</option>
+              <option value="Asia">Asia</option>
+              <option value="Afria">Africa</option>
+              <option value="Oceania">Oceania</option>
+              <option value="Americas">Americas</option>
+              <option value="Antarctic">Antarctic</option>
+            </select>
+          </form>
         </div>
-      ) : (
-        <h1 className="text-gray-800 font-bold uppercase tracking-wide flex items-center justify-center text-center h-screen text-4xl dark:text-white">
-          Loading...
-        </h1>
-      )}
-  </div>
+        {countries ? (
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+            {allCountries}
+          </div>
+        ) : (
+          <h1 className="text-gray-800 font-bold uppercase tracking-wide flex items-center justify-center text-center h-screen text-4xl dark:text-white">
+            Loading...
+          </h1>
+        )}
+      </div>
     </>
   );
 };
